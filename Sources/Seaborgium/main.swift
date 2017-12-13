be6808902f392e37480a7adb00401a7cede8f164
井川司 <tsukasa.ikawa@secual-inc.com>
2017-12-13 21:20:43 +0900

    雛形追加

--- Sources/Seaborgium/main.swift
+++ Sources/Seaborgium/main.swift
@@ -1,1 +1,50 @@
-print("Hello, world!")
+import Foundation
+import Aphid
+
+class Client: Aphid, MQTTDelegate {
+    
+    init(clientId: String) {
+        super.init(clientId: clientId)
+        super.delegate = self
+    }
+    
+    func didConnect() {
+        print("I connected!")
+    }
+    
+    func didLoseConnection() {
+        print("connection lost")
+    }
+    
+    func didCompleteDelivery(token: String) {
+        print(token)
+    }
+    
+    func didReceiveMessage(topic: String, message: String) {
+        print(topic, message)
+    }
+
+    func didLoseConnection(error: Error?) {
+        
+    }
+}
+
+let clientId = "Foo"
+
+let client = Client(clientId: clientId)
+
+try client.connect()
+
+client.subscribe(topic: ["city/+/temperature", "status/lastWill", "universe/+/+/planet"], qoss: [.atMostOnce, .atLeastOnce, .exactlyOnce])
+
+try client.ping()
+
+client.unsubscribe(topics: ["city/+/temperature", "universe/+/+/planet"])
+
+client.publish(topic: "city/austin/temperature", withMessage: "90 degrees")
+
+client.disconnect()
+
+while config.status == ConnectionStatus.connected {
+    sleep(60)
+}
